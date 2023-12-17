affd511f8345eaf911e1e4d4b11a9ca0ceddb3fb
olawale-o <omoogunolawale@gmail.com>
2023-12-17 21:09:31 +0100

    Update branches lib

--- src/app/branch/lib/branches.js
+++ src/app/branch/lib/branches.js
@@ -36,6 +36,7 @@ const branches = [
     subheading: "ogudu guesthouse",
     slug: "datanomics-guesthouse-ogudu",
     rooms: ["Standard", "Annex", "VIP", "Senate"],
+    link: "/branch/datanomics-guesthouse-ogudu",
     location: {
       lat: 6.581770446624733,
       lng: 3.385919266832977
@@ -71,11 +72,13 @@ const branches = [
         id: 1,
         url: Ogudu1,
         caption: '',
+        alt: "ogudu hotel"
       },
       {
         id: 2,
         url: OguduRoom,
         caption: '',
+        alt: "ogudu hotel"
       }
     ]
   },
@@ -85,6 +88,7 @@ const branches = [
     subheading: "onipanu guesthouse",
     slug: "datanomics-guesthouse-onipanu",
     rooms: ["Single", "Executive standard", "Royal suite", "Double twin", "Governor suite"],
+    link: "/branch/datanomics-guesthouse-onipanu",
     location: {
       lat: 6.5370591978876496,
       lng: 3.3682871668326397
@@ -119,12 +123,14 @@ const branches = [
       {
         id: 1,
         url: Onipanu1,
-        caption: ''
+        caption: '',
+        alt: "onipa hotelnu"
       },
       {
         id: 2,
         url: Onipanu2,
-        caption: ''
+        caption: '',
+        alt: "onipa hotelnu"
       },
     ]
   },
@@ -133,6 +139,7 @@ const branches = [
     name: "Parliament Hotel",
     slug: "parliament-hotel",
     rooms: ["Single", "Executive VIP", "Governor suite", "Royal suite"],
+    link: "/branch/parliament-hotel",
     location: {
       lat: 7.236438730252321,
       lng: 5.223838838003068
@@ -167,50 +174,146 @@ const branches = [
       {
         id: 1,
         url: Parliament1,
-        caption: ''
+        caption: '',
+        alt: "parli hotelament"
       },
       {
         id: 3,
         url: Parliament4,
-        caption: ''
+        caption: '',
+        alt: "parli hotelament"
       },
       {
         id: 4,
         url: Parliament5,
-        caption: ''
+        caption: '',
+        alt: "parli hotelament"
       },
       {
         id: 5,
         url: Parliament6,
-        caption: ''
+        caption: '',
+        alt: "parli hotelament"
       },
       {
         id: 6,
         url: Parliament10,
-        caption: ''
+        caption: '',
+        alt: "parli hotelament"
       },
       {
         id: 7,
         url: Parliament11,
-        caption: ''
+        caption: '',
+        alt: "parli hotelament"
       },
       {
         id: 8,
         url: Parliament12,
-        caption: ''
+        caption: '',
+        alt: "parli hotelament"
       },
       {
         id: 9,
         url: Parliament15,
-        caption: ''
+        caption: '',
+        alt: "parli hotelament"
       },
     ]
   },
   {
     id: 4,
+    name: "Ishua Hotel",
+    slug: "ishua-hotel",
+    link: "/branch/ishua-hotel",
+    rooms: ["Single", "Standard", "Executive", "VIP", "Royal suite 1", "Royal suite 2"],
+    location: {
+      lat: 7.449359392011563,
+      lng: 5.9086481489433655
+    },
+    facilities: [
+      {
+        icon: <AiFillBulb className={style.fill} />,
+        name: "24 hrs power supply"
+      },
+      {
+        icon: <BiSolidCctv className={style.fill}  />,
+        name: "Surveillance Cameras (CCTV)"
+      },
+      {
+        icon: <PiTelevisionSimpleFill className={style.fill}  />,
+        name: "Plasma Televsion"
+      },
+      {
+        icon: <FaSatelliteDish className={style.fill}  />,
+        name: "DSTV"
+      },
+      {
+        icon: <BiSolidFridge className={style.fill}  />,
+        name: "Bedroom Fridge"
+      },
+      {
+        icon: <LuAirVent className={style.fill}  />,
+        name: "Air Conditioning"
+      },
+    ],
+    images: [
+      {
+        id: 1,
+        url: IshuaStandardRoom,
+        caption: '',
+        alt: "ishua hotel"
+      },
+      {
+        id: 2,
+        url: IshuaHoterlEnviron,
+        caption: '',
+        alt: "ishua hotel"
+      },
+      {
+        id: 3,
+        url: IshuaRoom2,
+        caption: '',
+        alt: "ishua hotel"
+      },
+      {
+        id: 4,
+        url: IshuaRoom4,
+        caption: '',
+        alt: "ishua hotel"
+      },
+      {
+        id: 5,
+        url: IshuaRoyalSuite1,
+        caption: '',
+        alt: "ishua hotel"
+      },
+      {
+        id: 6,
+        url: IshuaRoyalSuite2,
+        caption: '',
+        alt: "ishua hotel"
+      },
+      {
+        id: 7,
+        url: IshuaHotelCorridor,
+        caption: '',
+        alt: "ishua hotel"
+      },
+      {
+        id: 8,
+        url: IshuaVip,
+        caption: '',
+        alt: "ishua hotel"
+      },
+    ]
+  },
+  {
+    id: 5,
     name: "Ugbeh Hotel",
     slug: "ugbeh-hotel",
     rooms: ["Standard", "Executive vip"],
+    link: "/branch/ugbeh-hotel",
     location: {
       lat: 7.525072203466108,
       lng: 5.7669265956769316
@@ -246,85 +349,8 @@ const branches = [
         id: 1,
         url: Ugbeh,
         caption: '',
+        alt: "ugbeh hotel"
       }
-    ]
-  },
-  {
-    id: 5,
-    name: "Ishua Hotel",
-    slug: "ishua-hotel",
-    rooms: ["Single", "Standard", "Executive", "VIP", "Royal suite 1", "Royal suite 2"],
-    location: {
-      lat: 7.449359392011563,
-      lng: 5.9086481489433655
-    },
-    facilities: [
-      {
-        icon: <AiFillBulb className={style.fill} />,
-        name: "24 hrs power supply"
-      },
-      {
-        icon: <BiSolidCctv className={style.fill}  />,
-        name: "Surveillance Cameras (CCTV)"
-      },
-      {
-        icon: <PiTelevisionSimpleFill className={style.fill}  />,
-        name: "Plasma Televsion"
-      },
-      {
-        icon: <FaSatelliteDish className={style.fill}  />,
-        name: "DSTV"
-      },
-      {
-        icon: <BiSolidFridge className={style.fill}  />,
-        name: "Bedroom Fridge"
-      },
-      {
-        icon: <LuAirVent className={style.fill}  />,
-        name: "Air Conditioning"
-      },
-    ],
-    images: [
-      {
-        id: 1,
-        url: IshuaHotelCorridor,
-        caption: '',
-      },
-      {
-        id: 2,
-        url: IshuaHoterlEnviron,
-        caption: '',
-      },
-      {
-        id: 3,
-        url: IshuaRoom2,
-        caption: '',
-      },
-      {
-        id: 4,
-        url: IshuaRoom4,
-        caption: '',
-      },
-      {
-        id: 5,
-        url: IshuaRoyalSuite1,
-        caption: '',
-      },
-      {
-        id: 6,
-        url: IshuaRoyalSuite2,
-        caption: '',
-      },
-      {
-        id: 7,
-        url: IshuaStandardRoom,
-        caption: '',
-      },
-      {
-        id: 8,
-        url: IshuaVip,
-        caption: '',
-      },
     ]
   },
 ];
