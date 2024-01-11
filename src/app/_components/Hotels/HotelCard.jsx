a3b3a5df9b813552e81ac5000b09939de00bfb75
olawale-o <omoogunolawale@gmail.com>
2024-01-11 18:50:27 +0100

    Use remote hosted image url

--- src/app/_components/Hotels/HotelCard.jsx
+++ src/app/_components/Hotels/HotelCard.jsx
@@ -6,7 +6,7 @@ import style from "./Hotels.module.css";
 const HotelCard = ({ branch }) => {
   return (
     <figure className={style.hotel_card}>
-      <Image src={branch.images[0].url} alt={branch.images[0].alt} className={style.hotel_image} fill />
+      <Image src={`https://ik.imagekit.io/a67cfp0fz/datanomics/${branch.images[0].url}`} alt={branch.images[0].alt} className={style.hotel_image} fill />
       <figcaption className={style.figcaption}>
         <h3 className={`h3 ${style.hotel_card_title}`}>{branch.name} {branch?.subheading && (<><br /> {branch.subheading} </>)} </h3>
         <Link href={branch.link} className={style.btn_view}>View More</Link>
