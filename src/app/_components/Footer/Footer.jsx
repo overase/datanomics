32825d9c7104be0e422436074f0b072f33a19761
olawale-o <omoogunolawale@gmail.com>
2024-03-15 04:10:47 +0100

    Fix footer styling

--- src/app/_components/Footer/Footer.jsx
+++ src/app/_components/Footer/Footer.jsx
@@ -7,33 +7,49 @@ const Footer = () => {
       <div className="container">
         <div className={style.footer_group}>
           <div className="col">
-            <h4 className={`h4 ${style.mb_2} ${style.white_heading}`}>Quick links</h4>
-            <ul className={style.quick_links}>
-              <li className={style.quick_links__item}>
-                <Link href="/" className={style.quick_links__link}>Home</Link>
-              </li>
-              <li className={style.quick_links__item}>
-                <Link href="/#" className={style.quick_links__link}>Branches</Link>
-              </li>
-              <li className={style.quick_links__item}>
-                <Link href="/contact" className={style.quick_links__link}>Contact</Link>
-              </li>
-            </ul>
+            <div className={style.footer_links}>
+              <div className={style.footer_links_container}>
+                <span className={style.footer_title}>Quick links</span>
+                <ul className={style.quick_links}>
+                  <li className={style.quick_links__item}>
+                    <Link href="/" className={style.quick_links__link}>Home</Link>
+                  </li>
+                  <li className={style.quick_links__item}>
+                    <Link href="/#" className={style.quick_links__link}>Branches</Link>
+                  </li>
+                  <li className={style.quick_links__item}>
+                    <Link href="/contact" className={style.quick_links__link}>Contact</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
           <div className={`col ${style.booking}`}>
-            <h4 className={`h4 ${style.white_heading} ${style.mb_2}`}>Booking</h4>
-            <p className="text-white">08033323907</p>
-            <p className="text-white">08023284000</p>
-          </div>
-          <div className={`col ${style.contact}`}>
-            <h4 className={`h4 ${style.white_heading} ${style.mb_2}`}>Contact</h4>
-            <p className="text-white">
-              booking@datanomicsnigeria.com
-            </p>
-            <p className="text-white">
-              13, Unity Close, Opposite Ogudu Police Station, Off Ogudu Road, Ojota, Lagos
-            </p>
-          </div>
+              <div className={style.footer_links}>
+                <span className={style.footer_title}>Booking</span>
+                <ul className={style.quick_links}>
+                  <li className={style.quick_links__item}>
+                    08033323907
+                  </li>
+                  <li className={style.quick_links__item}>
+                    08023284000
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className={`col ${style.booking}`}>
+              <div className={style.footer_links}>
+                <span className={style.footer_title}>Contact</span>
+                <ul className={style.quick_links}>
+                  <li className={style.quick_links__item}>
+                    booking@datanomicsnigeria.com
+                  </li>
+                  <li className={style.quick_links__item}>
+                    13, Unity Close, Opposite Ogudu Police Station, Off Ogudu Road, Ojota, Lagos
+                  </li>
+                </ul>
+              </div>
+            </div>
         </div>
       </div>
       <div className="container">
